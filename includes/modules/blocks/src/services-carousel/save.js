a1b5fb96b202e234362b7baede9bbdb7719c5d05
Reza Ardestani <re.ardestanii@gmail.com>
2026-07-02 20:16:32 +0330

    Dynamic block does ot need to save anything

--- includes/modules/blocks/src/services-carousel/save.js
+++ includes/modules/blocks/src/services-carousel/save.js
@@ -1,14 +1,3 @@
-import { useBlockProps } from '@wordpress/block-editor';
-
-export default function save( { attributes } ) {
-
-    const blockProps = useBlockProps.save( {
-        className: `svcl-container`
-    } );
-
-    return (
-        <div { ...blockProps }>
-            Services Carousel
-        </div>
-    );
+export default function save() {
+    return null;
 }
